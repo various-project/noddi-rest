7eb019ff834c53790e7d2a13abda03c45392d08a
Emil Schrøder <emilps@stud.ntnu.no>
2019-06-02 14:37:49 +0200

    Format and fix code errors

--- app.js
+++ app.js
@@ -1,51 +1,49 @@
-import express from "express";
+import express from 'express';
 
 import bodyParser from 'body-parser';
 
-import {firebaseDB} from './db/firebase';
+import firebaseDB from './db/firebase';
 
-const fs = require('fs');
+// const fs = require('fs');
 const http = require('http');
-const https = require('https');
+// const https = require('https');
 
 const app = express();
-app.use(express.static(__dirname, { dotfiles: 'allow' } ));
+app.use(express.static(__dirname, { dotfiles: 'allow' }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function (req, res, next) {
+/* eslint func-names: ["error", "never"] */
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+  // Pass to next layer of middleware
+  next();
 });
-
 
 app.get('/api/foods/:id', (req, res) => {
   firebaseDB.getFoodById(res, req.params.id);
-})
-
-
+});
 
 app.use((req, res) => {
-	res.send('Hello there !');
+  res.send('Hello there !');
 });
 
 const httpServer = http.createServer(app);
 
 httpServer.listen(80, () => {
-	console.log('HTTP Server running on port 80');
+  // eslint-disable-next-line no-console
+  console.log('HTTP Server running on port 80');
 });
