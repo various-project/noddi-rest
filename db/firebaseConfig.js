c2bc04e71e0fb526340d7c83113e1fb4ce2058d6
Emil Schrøder <emilps@stud.ntnu.no>
2019-06-04 22:11:13 +0200

    remove environment js

--- db/firebaseConfig.js
+++ db/firebaseConfig.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase';
 
-const Environment = require('./environment.js');
+// const Environment = require('./environment.js');
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
